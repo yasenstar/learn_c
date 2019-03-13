36512a1464d149b12d330a23f0be780e02021e53
yasenstar <xiaoqizhao@outlook.com>
2019-03-13 19:50:00 +0800

    updated

--- C_谭浩强/ch1/compare-to-bigger.c
+++ C_谭浩强/ch1/compare-to-bigger.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
     {
-        int max_num(int x, int y);
+        max(int x, int y);
         int a, b, c;
         scanf("%d,%d", &a, &b);
-        c = max_num(a,b);
+        c = max(a,b);
         printf("max=%d\n",c);
         return 0;
     }
