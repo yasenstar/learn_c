bae9fb6461f19ea2ea2cc004bb2dc2bc976419a9
Yasen Zhao <xiaoqizhao@outlook.com>
2020-10-17 08:24:18 -0400

    learning

--- cppprimer/02_开始学习C++/2.1_myfirst_no_namespace.cpp
+++ cppprimer/02_开始学习C++/2.1_myfirst_no_namespace.cpp
@@ -3,15 +3,12 @@
 #include <iostream>                             // a PREPROCESSOR directive
 int main(void)                                  // function header, it's same using main() or mina(void), normally main() is enough
 {                                               // start of function body
-    using namespace std;                        // make definitions visible, this is c++98 standard
-    cout << "Come up and C++ me some time.";    // message
-    cout << endl;                               // start a new line
-    cout << "You won't regret it!" << endl;     // more output
-    cout << "What's next\n";
-    cout << "Pluto is a dwarf planet.\n";
-    cout << "Pluto is a dwarf planet." << endl;
-    cout << "\n";
-    cout << endl;
+    // using namespace std;                        // make definitions visible, this is c++98 standard
+    std::cout << "Come up and C++ me some time.";    // message
+    std::cout << std::endl;                               // start a new line
+    std::cout << "You won't regret it!" << std::endl;     // more output
     // cin.get();
     return 0;                                   // terminate main()
 }                                               // end of function body
+
+// without namespace point to "std", need to use "std::" for all functions within STD, result is same
